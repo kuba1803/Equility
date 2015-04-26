e27412251ab0b1c41a09e884d8f94e74125f36db
kuba1803 <kuba1803@ymail.com>
2015-04-26 23:39:14 +0200

    GUI i kontroler
    W GUI nie ma jeszcze okienka wyboru kart i zakresow, Podpiolem już
    kontroler GUI oraz Stworzylem interfejs ktory powinismy zaimleentowac za
    posrednictwem niego bedziemy sciagac dane i zapisywac je do modelu

---
+++ Equility/src/main/java/Controller/MainControler.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author kuba1_000
+ */
+public interface MainControler {
+    public String getRange(String pos);
+    public String getScore();
+    public String setRange(String pos,String range);
+    public void Clear();
+    public boolean checkRange(String range);
+    public String getFloop();
+    public void setFloop(String floop);
+    public String getTurn();
+    public void setTurn(String turn);
+    public String getRiver();
+    public void SetRiver(String river);
+    
+}
---
+++ Equility/src/main/java/GUI/GUIController.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import javafx.scene.control.TextArea;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Pane;
+
+/**
+ *
+ * @author kuba1_000
+ */
+public class GUIController implements Initializable {
+
+    
+    Label actual;
+    @FXML
+    TextField Input;
+    @FXML
+    Pane Panel1;
+    @FXML
+    Pane Panel2;
+    @FXML
+    TextArea Output;
+
+    @FXML
+    private void handleButtonClear(ActionEvent event) {
+        System.out.print("Czysc");
+    }
+
+    @FXML
+    private void handleButtonComut(ActionEvent event) {
+        Output.setText("Obliczam");
+    }
+
+    @FXML
+    private void handleLabelPosition(MouseEvent event) {
+        System.out.print(event.getSource());
+        actual = (Label) event.getSource();
+        Panel1.setDisable(true);
+        Panel2.setVisible(true);
+        Panel2.setDisable(false);
+    }
+
+    @FXML
+    private void handlerConfirm(ActionEvent event) {
+        String str = Input.getText();
+        if(str.isEmpty())
+        {
+           actual.setStyle("-fx-border-width: 5; -fx-border-color: #000000; -fx-background-color: #8d8d8d");
+        }
+        else{
+        String pos = actual.getText();
+        switch(pos){
+            case "BB":{
+                actual.setStyle("-fx-border-width: 5; -fx-border-color: #000000; -fx-background-color: #ff0000");
+                break;
+            }
+            case "SB":{
+                actual.setStyle("-fx-border-width: 5; -fx-border-color: #000000; -fx-background-color: #ff0000");
+                break;
+            }
+            case "BU":{
+                actual.setStyle("-fx-border-width: 5; -fx-border-color: #000000; -fx-background-color: #5fff3b");
+                break;
+            }
+            case "CO":{
+                actual.setStyle("-fx-border-width: 5; -fx-border-color: #000000; -fx-background-color: #5fff3b");
+                break;
+            }
+            case "MP3":{
+                actual.setStyle("-fx-border-width: 5; -fx-border-color: #000000; -fx-background-color: #e1a81f");
+                break;
+            }
+            case "MP2":{
+                actual.setStyle("-fx-border-width: 5; -fx-border-color: #000000; -fx-background-color: #e1a81f");
+                break;
+            }
+        }
+        }
+        Panel2.setDisable(true);
+        Panel2.setVisible(false);
+        Panel1.setDisable(false);
+        Input.clear();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
